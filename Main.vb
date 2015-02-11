3906b585137c041bf27fb3c7f7fd2eec94c3e8f3
cstrobbe <c_strobbe-github@yahoo.co.uk>
2015-02-11 21:42:48 +0100

    Initial commit of STMM analysis module

---
+++ Main.vb
@@ -0,0 +1,203 @@
+﻿Module Main
+    'Arguments
+    Private Const _ArgConfigFile As String = "-c"
+    Private Const _ArgProfilesDirectory As String = "-p"
+    'Profiles
+    Private _ConfigFile As IO.FileInfo
+    Private _ProfilesDirectory As IO.DirectoryInfo
+    Private _Profiles As New List(Of Preferences.Profile)
+
+    Sub Main()
+        'Parse arguments
+        Dim args() As String = System.Environment.GetCommandLineArgs
+        Dim i As Integer = 1
+        While i < args.Count
+            Select Case args(i).Trim
+                Case _ArgProfilesDirectory
+                    If i < args.Count - 1 Then _ProfilesDirectory = New IO.DirectoryInfo(args(i + 1).TrimEnd("\"))
+                Case _ArgConfigFile
+                    If i < args.Count - 1 Then _ConfigFile = New IO.FileInfo(args(i + 1).Trim)
+            End Select
+            i += 2
+        End While
+        'Read Config
+        Clustering.ReadConfiguration(_ConfigFile)
+        'Preprocess Profiles
+        Dim generatedDirectory As New IO.DirectoryInfo(_ProfilesDirectory.FullName & "\generated")
+        If Not generatedDirectory.Exists Then generatedDirectory.Create()
+        PreprocessFrom(_ProfilesDirectory, generatedDirectory)
+        'Read Profiles
+        If _ConfigFile Is Nothing OrElse Not _ConfigFile.Exists Then Throw New NotSupportedException("Config File not set.")
+        If _ProfilesDirectory IsNot Nothing AndAlso _ProfilesDirectory.Exists Then
+            ReadProfilesFrom(_ProfilesDirectory)
+        Else
+            Throw New NotSupportedException("Preference Directory not set.")
+        End If
+        Preferences.GetMinsAndMaxs(_Profiles)
+        'Cluster
+        Dim clusters As List(Of HashSet(Of Preferences.Profile)) = Nothing
+        Dim noise As HashSet(Of Preferences.Profile) = Nothing
+        For Each clusterer In Clustering.Config
+            clusterer.Run(_Profiles, clusters, noise)
+        Next
+        'Start Processing
+        Dim generalized As New List(Of Preferences.Profile)
+        For Each c As HashSet(Of Preferences.Profile) In clusters
+            Dim center As Preferences.Profile = Nothing
+            Dim distances As SortedDictionary(Of Double, HashSet(Of Preferences.Profile)) = Nothing
+            Preferences.FindCenter(c, center, distances)
+            'generalized.Add(Preferences.GeneralizeProfile(center, distances, _Profiles))
+            generalized.Add(center)
+        Next
+        'Write JS
+        WriteJavaScript(generalized)
+    End Sub
+
+    Private Sub PreprocessFrom(directory As IO.DirectoryInfo, generatedDirectory As IO.DirectoryInfo)
+        If directory Is Nothing Then Exit Sub
+        If Not directory.Exists Then Exit Sub
+        For Each containedFile In directory.GetFiles
+            If containedFile.Extension = ".ini" And containedFile.Name.Contains(".gen") Then Parser.PreprocessProfile(containedFile, generatedDirectory)
+        Next
+        For Each containedDirectory In directory.GetDirectories
+            PreprocessFrom(containedDirectory, generatedDirectory)
+        Next
+    End Sub
+
+    Private Sub ReadProfilesFrom(directory As IO.DirectoryInfo)
+        If directory Is Nothing Then Exit Sub
+        If Not directory.Exists Then Exit Sub
+        For Each containedFile In directory.GetFiles
+            If containedFile.Extension = ".ini" And Not containedFile.Name.Contains(".gen") Then Parser.ReadProfile(containedFile, _Profiles)
+        Next
+        For Each containedDirectory In directory.GetDirectories
+            ReadProfilesFrom(containedDirectory)
+        Next
+    End Sub
+
+    Private Sub WriteJavaScript(clusters As List(Of Preferences.Profile))
+        Dim writer As New IO.StreamWriter(_ProfilesDirectory.FullName & "\StatisticalMatchMakerData.js", False)
+        writer.WriteLine("/*!")
+        writer.WriteLine()
+        writer.WriteLine("GPII/Cloud4all Statistical Matchmaker ")
+        writer.WriteLine()
+        writer.WriteLine("Copyright 2014 Hochschule der Medien (HdM) / Stuttgart Media University")
+        writer.WriteLine()
+        writer.WriteLine("Licensed under the New BSD License. You may not use this file except in")
+        writer.WriteLine("compliance with this licence.")
+        writer.WriteLine()
+        writer.WriteLine("You may obtain a copy of the licence at")
+        writer.WriteLine("https://github.com/REMEXLabs/GPII-Statistical-Matchmaker-Analysis/blob/master/LICENSE.txt")
+        writer.WriteLine()
+        writer.WriteLine("The research leading to these results has received funding from")
+        writer.WriteLine("the European Union's Seventh Framework Programme (FP7/2007-2013)")
+        writer.WriteLine("under grant agreement no. 289016.")
+        writer.WriteLine("*/")
+        writer.WriteLine()
+        writer.WriteLine("//Generated " & Now.ToString("dd MMM HH:mm:ss"))
+        writer.WriteLine("var fluid = fluid || require(""universal"");")
+        writer.WriteLine("var stat = fluid.registerNamespace(""gpii.matchMaker.statistical"");")
+
+        'entry count
+        writer.WriteLine("stat.entryCount = " & Preferences.EntryNames.Count & ";")
+
+        'clusters
+        writer.WriteLine("stat.clusters = [")
+        For c = 0 To clusters.Count - 1
+            Dim cluster As Preferences.Profile = clusters(c)
+            writer.WriteLine(vbTab & "{")
+            'sort by application
+            Dim prefsByApp As New Dictionary(Of String, HashSet(Of Preferences.Entry))
+            For n = 0 To Preferences.EntryNames.Count - 1
+                Dim entry As Preferences.Entry = Nothing
+                If cluster.PreferenceEntries.TryGetValue(Preferences.EntryNames(n), entry) Then
+                    Dim appPrefs As HashSet(Of Preferences.Entry) = Nothing
+                    If Not prefsByApp.TryGetValue(entry.Application, appPrefs) Then
+                        appPrefs = New HashSet(Of Preferences.Entry)
+                        prefsByApp(entry.Application) = appPrefs
+                    End If
+                    appPrefs.Add(entry)
+                End If
+            Next
+            'print
+            For Each pair In prefsByApp
+                writer.WriteLine(vbTab & vbTab & """" & pair.Key & """: {")
+                For n = 0 To pair.Value.Count - 1
+                    If n < pair.Value.Count - 1 Then
+                        writer.WriteLine(vbTab & vbTab & vbTab & WriteJavaScriptEntry(pair.Value(n)) & ",")
+                    Else
+                        writer.WriteLine(vbTab & vbTab & vbTab & WriteJavaScriptEntry(pair.Value(n)))
+                    End If
+                Next
+                If prefsByApp.Last.Key = pair.Key Then
+                    writer.WriteLine(vbTab & vbTab & "}")
+                Else
+                    writer.WriteLine(vbTab & vbTab & "},")
+                End If
+            Next
+            If c < clusters.Count - 1 Then writer.WriteLine(vbTab & "},") Else writer.WriteLine(vbTab & "}")
+        Next
+        writer.WriteLine("];")
+
+        'properties
+        writer.WriteLine("stat.preferenceTypes = {")
+        Dim typesByApp As New Dictionary(Of String, List(Of PreferenceType))
+        For n = 0 To Preferences.EntryNames.Count - 1
+            Dim entryName As String = Preferences.EntryNames(n)
+            Dim appPrefs As List(Of PreferenceType) = Nothing
+            If Not typesByApp.TryGetValue(Preferences.EntryApp(entryName), appPrefs) Then
+                appPrefs = New List(Of PreferenceType)
+                typesByApp(Preferences.EntryApp(entryName)) = appPrefs
+            End If
+            appPrefs.Add(New PreferenceType With {.name = entryName,
+                                                  .IsEnum = Preferences.IsEnumeration(entryName),
+                                                  .Min = Preferences.EntryMin(entryName),
+                                                  .Max = Preferences.EntryMax(entryName)})
+        Next
+        'print
+        For Each pair In typesByApp
+            writer.WriteLine(vbTab & """" & pair.Key & """: {")
+            For n = 0 To pair.Value.Count - 1
+                writer.WriteLine(vbTab & vbTab & """" & Preferences.EntryPrintName(pair.Value(n).name) & """: {")
+                If pair.Value(n).IsEnum Then
+                    writer.WriteLine(vbTab & vbTab & vbTab & """isEnum"": true,")
+                    writer.WriteLine(vbTab & vbTab & vbTab & """min"": 0,")
+                    writer.WriteLine(vbTab & vbTab & vbTab & """max"": " & Preferences.EnumerationSize(pair.Value(n).name))
+                Else
+                    writer.WriteLine(vbTab & vbTab & vbTab & """isEnum"": false,")
+                    writer.WriteLine(vbTab & vbTab & vbTab & """min"": " & pair.Value(n).Min & ",")
+                    writer.WriteLine(vbTab & vbTab & vbTab & """max"": " & pair.Value(n).Max)
+                End If
+                If n < pair.Value.Count - 1 Then
+                    writer.WriteLine(vbTab & vbTab & "},")
+                Else
+                    writer.WriteLine(vbTab & vbTab & "}")
+                End If
+            Next
+            If typesByApp.Last.Key = pair.Key Then
+                writer.WriteLine(vbTab & "}")
+            Else
+                writer.WriteLine(vbTab & "},")
+            End If
+        Next
+        writer.WriteLine("};")
+        writer.Close()
+    End Sub
+
+    Private Function WriteJavaScriptEntry(entry As Preferences.Entry) As String
+        If Preferences.IsEnumeration(entry) Then
+            Dim enumValue As String = Preferences.ToEnumeration(entry.Name, entry.Value)
+            If Not (enumValue.ToLower = "true" Or enumValue.ToLower = "false") Then enumValue = """" & enumValue & """"
+            Return """" & Preferences.EntryPrintName(entry.Name) & """: " & enumValue & ""
+        Else
+            Return """" & Preferences.EntryPrintName(entry.Name) & """: " & entry.Value & ""
+        End If
+    End Function
+
+    Private Structure PreferenceType
+        Public name As String
+        Public IsEnum As Boolean
+        Public Min, Max As Double
+    End Structure
+
+End Module
---
+++ Parser/Parser.vb
@@ -0,0 +1,114 @@
+﻿Module Parser
+    Private Const _TokenContext As String = "[context]"
+    Private Const _TokenPreferences As String = "[preferences]"
+
+    Public Sub ReadProfile(file As IO.FileInfo, profiles As List(Of Preferences.Profile))
+        Dim profile As New Preferences.Profile(file)
+        profiles.Add(profile)
+        'Read file
+        Dim parseMode As ParseMode = parseMode.None
+        Dim line, name, value As String
+        Dim application As String = String.Empty
+        Dim reader As New IO.StreamReader(file.FullName)
+        While Not reader.EndOfStream
+            line = reader.ReadLine.Trim
+            'Remove comments
+            Dim commentStart As Integer = line.IndexOf(";")
+            If commentStart >= 0 Then line = line.Remove(commentStart)
+            'Parse line
+            Select Case line.ToLower
+                Case _TokenContext
+                    parseMode = Parser.ParseMode.Context
+                Case _TokenPreferences
+                    parseMode = Parser.ParseMode.Preferences
+                    application = String.Empty
+                Case Else
+                    'Splitting
+                    If line.Contains("=") Then
+                        name = line.Split("=")(0).Trim
+                        value = line.Split("=")(1).Trim
+                    ElseIf line.Contains(":") Then
+                        name = line.Split(":")(0).Trim
+                        value = line.Split(":")(1).Trim
+                    Else
+                        Continue While
+                    End If
+                    'Parsing
+                    If name = "app" Then
+                        application = value.Trim("""")
+                    Else
+                        Select Case parseMode
+                            Case Parser.ParseMode.Context
+                                Dim newEntry As Preferences.Entry = Preferences.ToEntry("context", name, value)
+                                profile.ContextEntries(newEntry.Name) = newEntry
+                            Case Parser.ParseMode.Preferences
+                                Dim newEntry As Preferences.Entry = Preferences.ToEntry(application, name, value)
+                                profile.PreferenceEntries(newEntry.Name) = newEntry
+                        End Select
+                    End If
+            End Select
+        End While
+        reader.Close()
+        reader.Dispose()
+    End Sub
+
+    Public Sub PreprocessProfile(file As IO.FileInfo, output As IO.DirectoryInfo)
+        Dim reader As New IO.StreamReader(file.FullName)
+        Dim line As String
+        Dim steps As Integer = 0
+        Dim result As New Text.StringBuilder
+        Dim vars As New Dictionary(Of String, PreprocessorVars)
+        While Not reader.EndOfStream
+            line = reader.ReadLine.Trim
+            If String.IsNullOrEmpty(line) Then Continue While
+            If line.First = "|"c And line.Last = "|"c Then
+                line = line.Trim("|")
+                Dim split As String() = line.Split("="c)
+                If split.Count = 2 Then
+                    Dim key As String = split.First.Trim
+                    Dim value As String = split.Last.Trim
+                    If key = "steps" Then
+                        Integer.TryParse(value, steps)
+                    Else
+                        Dim varParams As String() = value.Split(";"c)
+                        If varParams.Count = 2 Then
+                            Dim start As Double = 0
+                            Dim stepInc As Double = 0
+                            Double.TryParse(varParams.First, start)
+                            Double.TryParse(varParams.Last, stepInc)
+                            vars("|" & key & "|") = New PreprocessorVars With {.start = start, .stepInc = stepInc}
+                        End If
+                    End If
+                End If
+            Else
+                result.Append(line)
+                result.Append(vbNewLine)
+            End If
+        End While
+        reader.Close()
+        reader.Dispose()
+
+        Dim resultContent As String = result.ToString
+        For i As Integer = 0 To steps - 1
+            Dim curResult As String = resultContent
+            For Each var In vars
+                curResult = curResult.Replace(var.Key, var.Value.start + var.Value.stepInc * i)
+            Next
+            Dim writer As New IO.StreamWriter(output.FullName & "\" & file.Name.Split(".").First & "_" & i & ".ini", False)
+            writer.Write(curResult)
+            writer.Close()
+            writer.Dispose()
+        Next
+    End Sub
+
+    Private Structure PreprocessorVars
+        Public start As Double
+        Public stepInc As Double
+    End Structure
+
+    Private Enum ParseMode As Byte
+        None
+        Context
+        Preferences
+    End Enum
+End Module
